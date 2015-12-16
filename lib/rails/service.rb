5b90c48cc9cd3a131eee78ec4f0846bbf751ddf6
Lukasz Strzalkowski <lukasz@squareup.com>
2015-12-16 09:39:15 -0800

    load_initializer loads rails' initializer for rails_service in the orderd we want

--- lib/rails/service.rb
+++ lib/rails/service.rb
@@ -26,6 +26,24 @@ module Rails
       @manifest   = Manifest.new(config._for_manifest)
       @app_config = AppConfig.new(config._for_app_config)
       @logger     = @config.logger
+
+      load_initializer!
+
+      nil
+    end
+
+    def configure
+      yield config
+    end
+
+    # We're doing out-of-band initializer loading here because we want it loaded
+    # before the regular rails init happens
+    # TODO(lukasz) add logging and maybe make initializer filename configurable?
+    def load_initializer!
+      unless @initializer_loaded
+        @initializer_loaded = true
+        require Rails.root.join('config/initializers/rails_service')
+      end
     end
   end
 end
